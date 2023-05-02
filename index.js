3ff6dbc79be2250cd639a2f96dd0f20a26138951
Yura Cepeda <yurandreac@gmail.com>
2023-05-02 17:22:27 -0500

    Updates

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import validator from './validator.js';
+const cardContainer = document.getElementById('card-container');
 const cardNumber = document.getElementById('card-number');
 const validateButton = document.getElementById("validateButton")
 const message = document.getElementById("message")
@@ -6,24 +7,37 @@ const messageIcon = document.getElementById("message-icon")
 const cardNumberRegex = /^\d+$/;
 validateButton.addEventListener("click", () => {  
   if(cardNumber.value === ""){
-    message.innerHTML = "Por favor ingrese el número de tarjeta que desea validar."
-    messageIcon.innerHTML = `⚠`  
+    cardContainer.classList = ""
+    cardContainer.classList.add("initialbg")
+    message.classList = ""
+    message.classList.add("yellow")
+    message.innerHTML = "Por favor ingrese el número de tarjeta que desea validar." 
+    messageIcon.innerHTML = `⚠`
   }else {
     if(cardNumberRegex.test(cardNumber.value)){
       const cardNumberArray = cardNumber.value.split('')
       const validatingCard = validator.isValid(cardNumberArray)
-      const numberMaskify = validator.maskify(cardNumberArray)
-        if(validatingCard){
-          message.innerHTML = `Su tarjeta número ${numberMaskify} es válida.`
-          messageIcon.innerHTML = `✔`
-        }else{
-          message.innerHTML = `Su tarjeta número ${numberMaskify}  no es válida.`
-          messageIcon.innerHTML = `❌`
-        }
-    } else {
+      const numberMaskify = validator.maskify(cardNumber.value)
+      if(validatingCard){
+        message.classList = ""
+        cardContainer.classList = ""
+        cardContainer.classList.add("success")
+        message.innerHTML = `Su tarjeta número ${numberMaskify} es válida.`
+        messageIcon.innerHTML = `✔`
+      }else{
+        message.classList = ""
+        cardContainer.classList = ""
+        cardContainer.classList.add("error")
+        message.innerHTML = `Su tarjeta número ${numberMaskify}  no es válida.`
+        messageIcon.innerHTML = `❌`
+      }
+    }else {
+      message.classList = ""
+      cardContainer.classList = ""
+      cardContainer.classList.add("warning")
       message.innerHTML = "Solo se puede validar números"
       messageIcon.innerHTML = `⚠`  
     }
   }
-cardNumber.value = ""
+  cardNumber.value = ""
 })
